04df2e0d0c73efc8f36097f8a8ee9c420eb76e5c
Jisue Jung <wjdwltn0909@gmail.com>
2024-07-12 10:39:05 +0900

    spring legacy sample code commit

---
+++ src/main/java/com/nc13/spring_legacy/controller/HomeController.java
@@ -0,0 +1,30 @@
+package com.nc13.spring_legacy.controller;
+
+import com.nc13.spring_legacy.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    private BoardService boardService;
+
+    @Autowired
+    public HomeController(BoardService boardService) {
+        this.boardService = boardService;
+    }
+
+    @GetMapping("/")
+    public String showHome(Model model) {
+        model.addAttribute("list", boardService.selectAll());
+        return "index";
+    }
+
+    @GetMapping("/board/showAll")
+    public String showAll(Model model) {
+        model.addAttribute("list", "showAll에 도착");
+        return "board/showAll";
+    }
+}
